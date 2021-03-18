f9000e6088ebe6167e8bc9614c31ad630888f96a
Jaden Eckel <jadeneckel@gmail.com>
2021-03-18 02:43:24 -0400

    Experimenting with setting up environment

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import HelloWorld from './HelloWorld'
+
+class Main extends Component {
+    render() {
+
+        return(
+            <>
+            <HelloWorld />
+            <HelloWorld />
+            <HelloWorld />
+            </>
+        );
+    }
+}
+
+export default Main;
